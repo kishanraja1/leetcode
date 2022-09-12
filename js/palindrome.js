c062475c36aae69d9061a104e3c0459723168dd0
Kishan Raja <kraja@experientgroup.com>
2022-09-12 14:38:10 -0400

    unfinished palindrome

---
+++ js/palindrome.js
@@ -0,0 +1,14 @@
+// PALINDROME - LEVEL EASY
+const palindrome = (x) => {
+    //set variable for number
+    let num = x
+    //reverse the number
+    let reverseNum = 0
+    //compare and print whther it is a palindrome
+    console.log(num)
+    console.log(reverseNum)
+
+
+}
+
+palindrome(121)
