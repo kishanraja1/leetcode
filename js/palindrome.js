b0302242d1d498f5346d1d16f52f2e8cdad53df0
Kishan Raja <kraja@experientgroup.com>
2022-09-13 10:27:44 -0400

    palindrome complete

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,13 +1,22 @@
 // PALINDROME - LEVEL EASY
 const palindrome = (x) => {
-    //set variable for number
-    let num = x
+    //set variable for number as an array
+    let num = x.toString().split("")
     //reverse the number
-    let reverseNum = 0
+    let reverseNum = []
     //compare and print whther it is a palindrome
-    console.log(num)
-    console.log(reverseNum)
+    for (let i = (num.length - 1); i >= 0; i--) {
+        reverseNum.push(num[i]) 
+    }
 
+    for (let i = 0; i < num.length; i++) {
+        if (num[i] != reverseNum[i]) {
+            console.log('it is not a palindrome')
+            return
+        }
+        
+    }
+    console.log('it is a palindrome')
 
 }
 
